fb051989f9594834331dc885c266362e5f1a7a7f
Ronald Pedid <ronaldpedid@live.com>
2017-02-05 15:38:56 -0600

    added ecosystem.js for pm2

---
+++ ecosystem.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: [{
+        name: 'www',
+        script: './bin/www',
+        watch: true,
+        env: {
+            NODE_ENV: 'production',
+            MONGO_CS: '45.55.135.114:27017'
+        },
+        env_production: {
+            NODE_ENV: 'production',
+            MONGO_CS: '45.55.135.114:27017'
+        }
+    }]
+}
