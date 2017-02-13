7c589c2584e1b8a1dcd5000b054ecf498252c856
Ronald Pedid <ronaldpedid@live.com>
2017-02-12 19:33:02 -0600

    working on index.hbs / style.css

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,11 @@ module.exports = {
         watch: true,
         env: {
             NODE_ENV: 'production',
-            MONGO_CS: 'mongodb://cgtc:"b92485c7-8d93-4a84-a9ed-a28015d4b2bd"@162.243.86.154:27017'
+            MONGO_CS: 'mongodb://162.243.86.154:27017'
         },
         env_production: {
             NODE_ENV: 'production',
-            MONGO_CS: 'mongodb://cgtc:"b92485c7-8d93-4a84-a9ed-a28015d4b2bd"@162.243.86.154:27017'
+            MONGO_CS: 'mongodb://162.243.86.154:27017'
         }
     }]
 };
